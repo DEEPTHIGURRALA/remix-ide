803ac7c91c990d5e220bfa28fa50cf4306027511
ninabreznik <ninabreznik@gmail.com>
2018-06-29 17:43:01 +0100

    Add filename to tests summary

--- src/app/tabs/test-tab.js
+++ src/app/tabs/test-tab.js
@@ -49,8 +49,9 @@ module.exports = class TestTab {
       cb()
     }
 
-    var updateFinalResult = function (_err, result) {
+    var updateFinalResult = function (_err, result, filename) {
       testsSummary.hidden = false
+      testsSummary.appendChild(yo`<div class=${css.summaryTitle}> ${filename} </div>`)
       if (result.totalPassing > 0) {
         testsSummary.appendChild(yo`<div>${result.totalPassing} passing (${result.totalTime}s)</div>`)
       }
@@ -71,7 +72,7 @@ module.exports = class TestTab {
           var runningTest = {}
           runningTest[testFilePath] = { content }
           remixTests.runTestSources(runningTest, testCallback, resultsCallback, (error, result) => {
-            updateFinalResult(error, result)
+            updateFinalResult(error, result, testFilePath)
             callback(error)
           }, api.importFileCb)
         }
